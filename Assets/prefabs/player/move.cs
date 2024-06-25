08a7735dd11152caca9c0aedbd7dfdc9215380e7
Typic-AL <120287651+Typic-AL@users.noreply.github.com>
2024-06-25 17:03:11 -0400

    Added simple grassland map and updated player

--- Assets/prefabs/player/move.cs
+++ Assets/prefabs/player/move.cs
@@ -5,15 +5,29 @@ using UnityEngine;
 public class move : MonoBehaviour
 {
     [SerializeField] private float speed;
-    [SerializeField] private VariableJoystick joystick;
+    [SerializeField] private FloatingJoystick joystick;
     [SerializeField] private Rigidbody rb;
     [SerializeField] private Animator animator;
 
-    [SerializeField] public Camera cam;
+    [SerializeField] private Camera cam;
+
+    private Vector3 lastPosition;
+
+    public void Start()
+    {
+        lastPosition = transform.position;
+    }
 
     public void FixedUpdate()
     {
-        rb.velocity = new Vector3(joystick.Horizontal * speed, rb.velocity.y, joystick.Vertical * speed);
+        if(joystick.Horizontal == 0 && joystick.Vertical == 0)
+        {
+            rb.velocity = new Vector3(Mathf.Lerp(rb.velocity.x, 0, .5f), rb.velocity.y, Mathf.Lerp(rb.velocity.z, 0, .2f));
+        }
+        else
+        {
+            rb.velocity = new Vector3(joystick.Horizontal * speed, rb.velocity.y, joystick.Vertical * speed);
+        }     
 
         if (joystick.Horizontal != 0 || joystick.Vertical != 0)
         {
@@ -24,6 +38,16 @@ public class move : MonoBehaviour
 
     public void Update()
     {
-        cam.transform.position = new Vector3(gameObject.transform.position.x, cam.transform.position.y, gameObject.transform.position.z - 4.24f);
+        cam.transform.position = new Vector3(gameObject.transform.position.x, cam.transform.position.y, gameObject.transform.position.z - 6.9f);
+
+        if (animator != null && Time.deltaTime > 0.0f)
+        {
+            float distanceTravelledSinceLastFrame = (transform.position - lastPosition).magnitude;
+            float distancePerSecond = distanceTravelledSinceLastFrame / Time.deltaTime;
+
+            animator.SetFloat("Speed", distancePerSecond);
+        }
+
+        lastPosition = transform.position;
     }
 }
