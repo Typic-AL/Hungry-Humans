58d733b88c50d60a0ae02c29070ab0ea5719bb73
Typic-AL <120287651+Typic-AL@users.noreply.github.com>
2024-06-26 21:32:07 -0400

    Smoothed Out Growing and Camera Movement

--- Assets/prefabs/player/Grow.cs
+++ Assets/prefabs/player/Grow.cs
@@ -7,23 +7,52 @@ public class Grow : MonoBehaviour
     public int size = 0;
     public int level = 0;
     public float scaleFactor;
+    public float scaleDuration;
 
     [SerializeField] private Camera cam;
     private float camY;
+    private float camTargetY;
+    private float camInitY;
     private float camZ;
+    private float camTargetZ;
+    private float camInitZ;
+
+    private Vector3 scale;
+    private Vector3 targetScale;
+    private bool canGrow = false;
+    private float timeElapsed = 0;
     
     
     // Start is called before the first frame update
     void Start()
     {
-        camY = cam.transform.position.y;
-        camZ = 6.9f;
+        camTargetY = cam.transform.position.y;
+        camY = camTargetY;
+        camInitY = camY;
+        camTargetZ = 6.9f;
+        camZ = camTargetZ;
+        camInitZ = camZ;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(level == 0)
+        if(canGrow)
+        {
+            if (timeElapsed <= scaleDuration)
+            {
+                timeElapsed += Time.deltaTime;
+                float progress = timeElapsed / scaleDuration;
+                transform.localScale = Vector3.Lerp(scale, targetScale, progress);
+                camY = Mathf.Lerp(camInitY, camTargetY, progress);
+                camZ = Mathf.Lerp(camInitZ, camTargetZ, progress);
+            }
+            else
+                canGrow = false;
+        }
+
+        if (level == 0)
             cam.transform.position = new Vector3(gameObject.transform.position.x, cam.transform.position.y, gameObject.transform.position.z - 6.9f);
         else
             cam.transform.position = new Vector3(gameObject.transform.position.x, camY, gameObject.transform.position.z - camZ);
@@ -55,14 +84,18 @@ public class Grow : MonoBehaviour
 
     private void grow()
     {
-        Vector3 ts = transform.localScale;
+        timeElapsed = 0;
+        scale = transform.localScale;
+        targetScale = new Vector3 (scale.x * scaleFactor, scale.y * scaleFactor, scale.z * scaleFactor);
         if (size >= 10*(level + 1)*.75)
         {
-            transform.localScale = new Vector3(scaleFactor * ts.x, scaleFactor * ts.y, scaleFactor * ts.z);
+            canGrow = true;
             level += 1;
             size = 0;
-            camY *= scaleFactor;
-            camZ *= scaleFactor;
+            camInitY = cam.transform.position.y;
+            camInitZ = camZ;
+            camTargetY *= scaleFactor;
+            camTargetZ *= scaleFactor;
         }
     }
 }
